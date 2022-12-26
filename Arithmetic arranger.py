fa9c913656b94a0a7e3a71a60d957a82ec926009
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2022-12-26 10:22:18 +0300

    Update Arithmetic arranger.py

--- Arithmetic arranger.py
+++ Arithmetic arranger.py
@@ -1,3 +1,5 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
 # Create a function that receives a list of strings that are arithmetic
 # problems and returns the problems arranged vertically and side-by-side.
 # The function should optionally take a second argument and
@@ -7,58 +9,58 @@ import operator
 
 ops = {"+": operator.add, "-": operator.sub}
 
-
-def arithmetic_arranger(list_of_problems, solver=False):
-    """
+def arithmetic_arranger(list_of_problems,solver = False):
+ """
 
     :param solver:
     :type list_of_problems: list
     """
-    first_line = ""
-    second_line = ""
-    dashes = ""
+ first_line = ""
+ second_line = ""
+ dashes = ""
+ if len(list_of_problems) > 5:  # Function must only accept a maximum of five problems
+    
+    raise ValueError ('Error: Too many problems')
+ for problems in list_of_problems:
 
-    if len(list_of_problems) > 5:  # Function must only accept a maximum of five problems
+    problems = problems.replace(' ', '')
+    if not "+" or "-" not in problems:  # Second Error Handling: Only '+' and '-' operators are allowed
+        raise ValueError('Error: Operator must be \'+\' or \'-\'.')
 
-        return 'Error: Too many problems'
-    for problems in list_of_problems:
-
-        problems = problems.replace(' ', '')
-        if not "+" or "-" not in problems:  # Second Error Handling: Only '+' and '-' operators are allowed
-            return 'Error: Operator must be \'+\' or \'-\'.'
-
-        else:
+    else:
             first_number = problems.split()[0]
             operator = problems.split()[1]
             second_number = problems.split()[2]
 
-        if not first_number.isdigit() and not second_number.isdigit():
+    if not first_number.isdigit() and not second_number.isdigit():
 
-            return 'Error: Numbers must only contain digits.'  # Third Error Handling: Numbers must only contain digits
-        else:
+      raise ValueError('Error: Numbers must only contain digits.')# Third Error Handling: Numbers must only contain digits
+    else:
             pass
-        if abs(first_number) and abs(second_number) > 1e4:  # Fourth Error Handling: Numbers must only contain 4 digits
-            raise ValueError('Numbers must only contain 4 digits')
-        else:
+    if abs(first_number) and abs(second_number) > 1e4:  # Fourth Error Handling: Numbers must only contain 4 digits
+        raise ValueError('Numbers must only contain 4 digits')
+    else:
             pass
-        aligning_space = max([len(first_number), len(second_number)]) + 2
+    aligning_space = max([len(first_number), len(second_number)]) + 2
         # Getting number of digits contained in the largest  operand and adding 2 to it
         # This sets the number of characters that a line can have in each arranged expression
 
-        total = ops[operator](int(first_number), int(second_number))
+    total = ops[operator](int(first_number), int(second_number))
 
-        second_number = operator + second_number.rjust(aligning_space - 1)
-        first_line = first_line + first_number.rjust(aligning_space) + (4 * " ")
-        second_line = second_line + second_number + (4 * " ")
-        dashes = dashes + aligning_space * '_' + (4 * " ")
-        total = str(total).rjust(aligning_space) + (4 * " ")
-    if solver:
-        print(first_line)
-        print(second_line)
-        print(dashes)
-        print(total)
-        print(dashes)
-
+    second_number = operator + second_number.rjust(aligning_space - 1)
+    first_line = first_line + first_number.rjust(aligning_space) + (4 * " ")
+    second_line = second_line + second_number + (4 * " ")
+    dashes = dashes + aligning_space * '_' + (4 * " ")
+    total = str(total).rjust(aligning_space) + (4 * " ")
+    
+ arranged_problems = f"""
+{first_line}
+{second_line}
+{dashes}
+{total}"""
+        
+ return arranged_problems
+   
 
 if __name__ == "__main__":
     arithmetic_arranger([
