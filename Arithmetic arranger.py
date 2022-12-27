baf490a75fa5d9322d8d57cc13914342400982c6
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2022-12-27 20:50:32 +0300

    Update Arithmetic arranger.py

--- Arithmetic arranger.py
+++ Arithmetic arranger.py
@@ -1,5 +1,4 @@
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
+
 # Create a function that receives a list of strings that are arithmetic
 # problems and returns the problems arranged vertically and side-by-side.
 # The function should optionally take a second argument and
