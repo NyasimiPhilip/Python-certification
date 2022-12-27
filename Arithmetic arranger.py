e269f7af5914ebad6a2dbc9aecc24c2a5544ea20
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2022-12-27 23:35:17 +0300

    Update Arithmetic arranger.py

--- Arithmetic arranger.py
+++ Arithmetic arranger.py
@@ -1,4 +1,5 @@
-
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
 # Create a function that receives a list of strings that are arithmetic
 # problems and returns the problems arranged vertically and side-by-side.
 # The function should optionally take a second argument and
@@ -14,35 +15,42 @@ def arithmetic_arranger(list_of_problems, solver=False):
     :type list_of_problems: list
     """
     if len(list_of_problems) > 5:  # Function must only accept a maximum of five problems
-        raise ValueError ('Error: Too many problems')
+        return 'Error: Too many problems.'
 
     first_line = "" # To hold the first line of each of the expressions (i.e the line containing the first operand)
     second_line = ""# To hold the second line of each of the expressions (i.e the line containing the operator and second operand)
     dashes = ""# Bunch of horizontal dashes to separate the operands from their result)
     results = ""
-    aligning_space = 0
+    max_result_length = 0
     for problems in list_of_problems:
        
-        if "+" not in problems and "-" not in problems:  # Second Error Handling: Only '+' and '-' operators are allowed
-            raise ValueError('Error: Operator must be \'+\' or \'-\'.')
+        if "+" not in problems and  "-" not in problems:  # Second Error Handling: Only '+' and '-' operators are allowed
+            return 'Error: Operator must be '+' or '-'.'
 
         first_number = problems.split()[0]
         operator = problems.split()[1]
         second_number = problems.split()[2]
 
         if not first_number.isdigit() or not second_number.isdigit():  # Third Error Handling: Numbers must only contain digits
-            raise ValueError('Error: Numbers must only contain digits.')
+            return 'Error: Numbers must only contain digits.'
 
         if len(first_number or second_number) > 4:  # Fourth Error Handling: Numbers must only contain 4 digits
-            raise ValueError('Numbers must only contain 4 digits')
+            return 'Error: Numbers cannot be more than four digits.'
 
         result = ops[operator](int(first_number), int(second_number))
         aligning_space = max([len(first_number), len(second_number)]) + 2
+        if problems != list_of_problems[-1]:
+          first_line = first_line + first_number.rjust( aligning_space) + (4 * " ")
+          second_line = second_line + operator + second_number.rjust(aligning_space - 1) + (4 * " ")
+          dashes = dashes + aligning_space * '-' + (4 * " ")
+          results = results + str(result).rjust(aligning_space) + (4 * " ")
+        else:
+          first_line = first_line + first_number.rjust( aligning_space)
+          second_line = second_line + operator + second_number.rjust(aligning_space - 1) 
+          dashes = dashes + aligning_space * '-' 
+          results = results + str(result).rjust(aligning_space)   
 
-        first_line = first_line + first_number.rjust( aligning_space) + (4 * " ")
-        second_line = second_line + operator + second_number.rjust(aligning_space - 1) + (4 * " ")
-        dashes = dashes + aligning_space * '_' + (4 * " ")
-        results = results + str(result).rjust(aligning_space) + (4 * " ")
+        
 
     arranged_problems = f"{first_line}\n{second_line}\n{dashes}"
     if solver:
