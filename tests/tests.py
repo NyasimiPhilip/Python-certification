527c498a0069b7f9995537bda0a79bb86b113587
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-07-18 00:43:26 +0300

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -81,3 +81,6 @@ def test_template(arguments, expected_output, fail_message):
     actual = arithmetic_arranger(*arguments)
     assert actual == expected_output, fail_message
     print("Test passed successfully for:", arguments)
+
+if __name__ == "__main__":
+    pytest.main(["-s", __file__])
