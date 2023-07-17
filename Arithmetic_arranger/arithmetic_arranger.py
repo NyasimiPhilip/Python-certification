f1a6b53976f7fcf8686671e8cbcd7f1e03224ee5
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-07-18 00:29:00 +0300

    Update arithmetic_arranger.py

--- Arithmetic_arranger/arithmetic_arranger.py
+++ Arithmetic_arranger/arithmetic_arranger.py
@@ -1,7 +1,9 @@
-# Create a function that receives a list of strings that are arithmetic
-# problems and returns the problems arranged vertically and side-by-side.
-# The function should optionally take a second argument and
-# when the second argument is set to `True`, the answers should be displayed'''
+"""
+Create a function that receives a list of strings that are arithmetic
+problems and returns the problems arranged vertically and side-by-side.
+The function should optionally take a second argument and
+when the second argument is set to `True`, the answers should be displayed
+"""
 
 import operator
 import re
